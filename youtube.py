11f9c224a42ebf6fa1b4d1bb9ca2d3fa4aa80ae0
hayleybts <98275430+hayleybts@users.noreply.github.com>
2022-08-20 18:05:51 +0530

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,12 @@
+import pytube
+#the location you want to save
+save = r"C:\Users\--\Documents"
+#video file to be downloaded
+link = input('Enter the link you want to download')
+
+yt = pytube.YouTube(link)
+yt.streams.filter(progressive = True,
+file_extension = "mp4").first().download(save)
+
+#tite of file being downloaded
+print(yt.title)
